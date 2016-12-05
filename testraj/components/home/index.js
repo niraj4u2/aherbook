758de1c84647c3bce3baa59d9f4e947b45cbe0e4
vinod gahlot <vinod.webappmate@gmail.com>
2016-12-05 07:06:31 +0000

    Changes made by vinod gahlot (vinod.webappmate@gmail.com).

--- testraj/components/home/index.js
+++ testraj/components/home/index.js
@@ -4,6 +4,7 @@ app.home = kendo.observable({
     onShow: function() {},
     afterShow: function() {}
 });
+app.localization.registerView('home');
 
 // START_CUSTOM_CODE_home
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
--- testraj/upload_2dec/js/config.js
+++
@@ -1,5 +0,0 @@
-/* Config js is used for custom setting*/
-var BaseUrl = 'http://aherbook.webappmate.in/index.php/api_v1/webservice/';
-
-
-
